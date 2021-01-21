24ae7dbcde1092e53b24569289470b5250a23864
Erik <erik10cavazos@gmail.com>
2021-01-20 21:10:11 -0600

    agrego seccion de leer más al blog

--- Pages/leer.cshtml.cs
+++ Pages/leer.cshtml.cs
@@ -11,22 +11,27 @@ namespace erik_tech.Pages
 {
     public class LeerModel : PageModel
     {
-        private readonly DbContextApp contexto;
+        private readonly DbContextApp db;
         public Articulo articulo;
+        public List<Articulo> recommendedArticles;
         public bool error = false;
         public LeerModel(DbContextApp _contexto)
         {
-            contexto = _contexto;
+            db = _contexto;
         }
         public IActionResult OnGet(string id)
         {
             try
             {
                 ViewData["isArticle"] = true;
-                articulo = contexto.articulo.Where(a => a.Id.Equals(int.Parse(id))).Single();
+                articulo = db.articulo.Single(a => a.Id.Equals(int.Parse(id)));
                 ViewData["title"] = articulo.titulo_publicacion;
                 ViewData["description"] = MetodosEstaticoGeneralesErikTech.GetDescription(articulo.cuerpo);
                 ViewData["pictureURL"] = MetodosEstaticoGeneralesErikTech.GetAnImageURLFromHTML(articulo.cuerpo);
+                recommendedArticles = db.articulo
+                    .Where(articulo => !articulo.Id.Equals(this.articulo.Id)).ToList();
+                recommendedArticles.Reverse();
+                recommendedArticles = recommendedArticles.Take(5).ToList();
             }
             catch (Exception e)
             {
@@ -38,7 +43,7 @@ namespace erik_tech.Pages
         }
         public string BuscarNombreAutorPorIdArticulo(int id)
         {
-            var autor = contexto.cuenta.Where(c => c.Id.Equals(id)).Single();
+            var autor = db.cuenta.Where(c => c.Id.Equals(id)).Single();
             return autor.username;
         }
         
